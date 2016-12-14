d9789a7810338e060ed013f316580c0b4155df37
Hizzah <hizzah000@gmail.com>
2016-12-14 15:22:13 -0600

    JS Logic to add/remove Items

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,8 +3,14 @@ $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("active");
 });
-
+num = 0
 $("#CreateNew").click(function(e) {
-        console.log("Adding new Menu");            
-        $("#grabME").after("<li><a>Link1<svg class=\"icon icon-link\"><use xlink:href=\"symbol-defs.svg#icon-link\"></use></svg></a></li>")
+    num += 1;    
+    console.log("Adding new Menu" + num);            
+        $("#grabME").after("<div id=\"item"+num+"\" ><li><a onclick=\"removeItem("+num+")\">Link"+num+"<svg class=\"icon icon-link\"><use xlink:href=\"symbol-defs.svg#icon-link\"></use></svg></a></li></div>");
+        
 });
+function removeItem(num) {
+    item = "item"+num
+    $("#"+item).remove()
+}
