d11788ce6bdd8d50cc747a02c6402a3ace9bbd44
Hizzah <hizzah000@gmail.com>
2016-12-14 14:56:48 -0600

    Icon madness.

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,5 +1,10 @@
 $("#menu-toggle").click(function(e) {
+        console.log("Toggling Menu");
         e.preventDefault();
         $("#wrapper").toggleClass("active");
 });
 
+$("#CreateNew").click(function(e) {
+        console.log("Adding new Menu");            
+        $("#grabME").after("<li>Link1<a><span class=\"sub_icon icon-link\"></span></a></li>")
+});
