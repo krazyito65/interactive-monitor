9ff5861633489a373c2d8b8af8825ca2632e8e82
Hizzah <hizzah000@gmail.com>
2016-12-14 16:42:29 -0600

    Hit it with that cleanup.

--- js/sidebar.js
+++ js/sidebar.js
@@ -8,10 +8,10 @@ num = 0
 $("#CreateNew").click(function(e) {
     num += 1;    
     console.log("Adding new Menu: item");            
-        $("#grabME").after("<div id=\"item"+num+"\" ><li><a onclick=\"removeItem("+num+")\">Link"+num+"<span class=\"sub_icon glyphicon glyphicon-link\"></span></a></li></div>");
+        $("#grabME").after("<div id=\"item"+num+"\" ><li><a onclick=\"removeItem("+num+")\">Link"+num+"<span class=\"sub_icon glyphicon glyphicon-minus\"></span></a></li></div>");
 });
 
-function removeItem() {
+function removeItem(num) {
     item = "item"+num;
     console.log("Removing Item");
     $("#"+item).remove();
