d5a4fb4c4689d35c4f4a326c9f37d0f1432b46e0
Leon Solis <Leon.Solis@wnco.com>
2016-12-14 16:14:57 -0600

    trying to fix jacob

--- js/sidebar.js
+++ js/sidebar.js
@@ -8,8 +8,7 @@ num = 0
 $("#CreateNew").click(function(e) {
     num += 1;    
     console.log("Adding new Menu: item");            
-        $("#grabME").after("<div id=\"item"+num+"\" ><li><a onclick=\"removeItem("+item+")\">Link"+num+"<svg class=\"icon icon-link\"><use xlink:href=\"symbol-defs.svg#icon-link\"></use></svg></a></li></div>");
-        
+        $("#grabME").after("<div id=\"item"+num+"\" ><li><a onclick=\"removeItem("+num+")\">Link"+num+"<span class=\"sub_icon glyphicon glyphicon-link\"></span></a></li></div>");
 });
 
 function removeItem() {
