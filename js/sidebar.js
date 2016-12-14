98fa84b4ba6f81f0fb34387dcdaaad0225db27ba
Hizzah <hizzah000@gmail.com>
2016-12-14 16:05:25 -0600

    BAD Remove Logic YAYgit add *

--- js/sidebar.js
+++ js/sidebar.js
@@ -4,13 +4,17 @@ $("#menu-toggle").click(function(e) {
         $("#wrapper").toggleClass("active");
 });
 num = 0
+
 $("#CreateNew").click(function(e) {
     num += 1;    
-    console.log("Adding new Menu" + num);            
-        $("#grabME").after("<div id=\"item"+num+"\" ><li><a onclick=\"removeItem("+num+")\">Link"+num+"<svg class=\"icon icon-link\"><use xlink:href=\"symbol-defs.svg#icon-link\"></use></svg></a></li></div>");
+    console.log("Adding new Menu: item");            
+        $("#grabME").after("<div id=\"item"+num+"\" ><li><a onclick=\"removeItem("+item+")\">Link"+num+"<svg class=\"icon icon-link\"><use xlink:href=\"symbol-defs.svg#icon-link\"></use></svg></a></li></div>");
         
 });
-function removeItem(num) {
-    item = "item"+num
-    $("#"+item).remove()
+
+function removeItem() {
+    item = "item"+num;
+    console.log("Removing Item");
+    $("#"+item).remove();
+    this.num -= 1;
 }
