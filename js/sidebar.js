56d8666db5ee9269b2940348fa4f0e05bbd1788d
Leon Solis <Leon.Solis@wnco.com>
2016-12-14 18:19:50 -0600

    Fixed 'click whole section' problem and now only the icon is clickable

--- js/sidebar.js
+++ js/sidebar.js
@@ -8,7 +8,7 @@ num = 0
 $("#CreateNew").click(function(e) {
     num += 1;    
     console.log("Adding new Menu: item");            
-        $("#grabME").after("<div id=\"item"+num+"\" ><li><a onclick=\"removeItem("+num+")\">Link"+num+"<span class=\"sub_icon glyphicon glyphicon-minus\"></span></a></li></div>");
+        $("#grabME").after("<div id=\"item"+num+"\" ><li>Link"+num+"<a onclick=\"removeItem("+num+")\"><span class=\"sub_icon glyphicon glyphicon-minus\"></span></a></li></div>");
 });
 
 function removeItem(num) {
